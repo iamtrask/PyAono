643c9e69662a1bd4fe2401cfff4c6ee767f71812
Mayank <mayank.cse14@itbhu.ac.in>
2017-09-21 07:55:45 +0530

    Key switching example

--- init_key_switching.py
+++ init_key_switching.py
@@ -4,16 +4,26 @@ from Aono import *
 pari_init(2000000000, 2)
 keysgen = key_gen()
 keys1 = keysgen.generate_key(100, 64, 40, 8, 4, 7)
-
-
-ct = ciphertext(create_message_matrix(26, 64), keys1.pk, keys1.pk.params)
+message = 33
+print "Plaintext message: " + str(message)
+ct = ciphertext(create_message_matrix(message, 64), keys1.pk, keys1.pk.params)
 
 keys2 = keysgen.generate_key(100, 64, 20, 16, 4, 7)
-
+keys3 = keysgen.generate_key(100, 64, 60, 16, 4, 7)
 ukgen = updation_key_gen()
+
+print "Reducing security with key switching"
 uk = ukgen.generate_key(keys1, keys2)
 a = uk.cipher_switch(ct)
+print "Decrypted message: "
 print_GEN(get_element(get_element(a.decrypt(keys2.sk), 0), 0))
+
+print "Increasing security with key switching"
+uk = ukgen.generate_key(keys1, keys3)
+a = uk.cipher_switch(ct)
+print "Decrypted message: "
+print_GEN(get_element(get_element(a.decrypt(keys3.sk), 0), 0))
+
 pari_close()
 
 
